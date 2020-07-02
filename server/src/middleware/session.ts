891cca38f1cf3028cdd0c37d0912428f99928a85
Steeely Dan <steeelydan@posteo.de>
2020-07-02 16:25:14 +0200

    Express session cookie: use maxAge instead of expiration date

--- server/src/middleware/session.ts
+++ server/src/middleware/session.ts
@@ -22,7 +22,7 @@ const configureSession = (
         cookie: {
             sameSite: true,
             httpOnly: true,
-            expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+            maxAge: 7 * 24 * 60 * 60 * 1000,
             secure: process.env.NODE_ENV === 'production'
         }
     });
