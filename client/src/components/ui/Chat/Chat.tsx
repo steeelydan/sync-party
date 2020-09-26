6f2f0b16f75ad03a4aed23f020dea69f07ea1859
Steeely Dan <steeelydan@posteo.de>
2020-09-27 00:45:16 +0200

    Quit freezing ui when escaping from chat input

--- client/src/components/ui/Chat/Chat.tsx
+++ client/src/components/ui/Chat/Chat.tsx
@@ -67,6 +67,7 @@ export default function Chat({
                 setShowEmojiPicker(false);
             } else {
                 setIsActive(false);
+                freezeUiVisible(false);
             }
         }
     };
