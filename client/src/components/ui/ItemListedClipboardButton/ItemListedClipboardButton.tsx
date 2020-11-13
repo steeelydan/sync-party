62b8b9532105f25851e6a38da1f894e97d06d14e
Steeely Dan <steeelydan@posteo.de>
2020-11-13 21:10:10 +0100

    Animate clipboard copy button

--- client/src/components/ui/ItemListedClipboardButton/ItemListedClipboardButton.tsx
+++ client/src/components/ui/ItemListedClipboardButton/ItemListedClipboardButton.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
 import CopyToClipboard from 'react-copy-to-clipboard';
 import { useTranslation } from 'react-i18next';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -14,16 +14,24 @@ export default function ItemListedClipboardButton({
     hovering
 }: Props): ReactElement {
     const { t } = useTranslation();
+    const [isClicked, setIsClicked] = useState(false);
 
     return (
         <CopyToClipboard text={itemUrl}>
-            <span
-                className={!hovering ? 'hidden' : 'mr-2 my-auto'}
+            <div
+                onClick={(): void => setIsClicked(true)}
+                onAnimationIteration={(): void => {
+                    setIsClicked(false);
+                }}
+                className={
+                    'text-gray-300 hover:text-white' +
+                    (!hovering ? ' hidden' : ' mr-2 my-auto') +
+                    (isClicked ? ' animate-ping' : '')
+                }
                 title={t('mediaMenu.copy')}
-                color="text-gray-300 hover:text-gray-200"
             >
                 <FontAwesomeIcon icon={faClipboard} size="1x"></FontAwesomeIcon>
-            </span>
+            </div>
         </CopyToClipboard>
     );
 }
