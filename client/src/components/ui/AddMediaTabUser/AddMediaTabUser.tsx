da5780f3050cceed1a77d6a0f8f2e4045a97c308
Steeely Dan <steeelydan@posteo.de>
2020-11-17 01:07:47 +0100

    Get react-virtualized running

--- client/src/components/ui/AddMediaTabUser/AddMediaTabUser.tsx
+++ client/src/components/ui/AddMediaTabUser/AddMediaTabUser.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useEffect, useState } from 'react';
+import React, { ReactElement, useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSadCry } from '@fortawesome/free-solid-svg-icons';
@@ -9,7 +9,7 @@ import {
     CellMeasurer,
     CellMeasurerCache
 } from 'react-virtualized';
-import ItemListed2 from '../ItemListed2/ItemListed2';
+import ItemListed from '../ItemListed/ItemListed';
 
 interface Props {
     partyItemsSet: Set<string>;
@@ -34,17 +34,29 @@ export default function AddMediaTabUser({
         });
     };
 
+    const refilterItems = () => {
+        if (userItems) {
+            setFilteredItems(
+                sortItems(
+                    userItems.filter((userItem) => {
+                        return !partyItemsSet.has(userItem.id);
+                    })
+                )
+            );
+        }
+    };
+
     const { t } = useTranslation();
     const [itemsFilter, setItemsFilter] = useState<string | null>(null);
     const [filteredItems, setFilteredItems] = useState<MediaItem[]>(
         userItems && userItems.length ? sortItems(userItems) : []
     );
-    const [listCache, setListCache] = useState(
+    const listCache = useRef(
         new CellMeasurerCache({
             fixedWidth: true,
             defaultHeight: 50
         })
-    ); // FIXME
+    );
 
     // (Re-)Filter items if items filter term or user items change
     useEffect(() => {
@@ -69,8 +81,6 @@ export default function AddMediaTabUser({
         }
     }, [userItems, itemsFilter]);
 
-    console.log(listCache);
-
     return (
         <div>
             <input
@@ -82,44 +92,62 @@ export default function AddMediaTabUser({
                 onBlur={(): void => setPlayerFocused(true)}
             ></input>
             {filteredItems.length ? (
-                <div className="">
+                <div className="userItemList">
                     <AutoSizer>
                         {({ width, height }): React.ReactElement => {
                             return (
                                 <List
                                     width={width}
                                     height={height}
-                                    deferredMeasurementCache={listCache}
-                                    rowHeight={listCache.rowHeight}
+                                    deferredMeasurementCache={listCache.current}
+                                    rowHeight={listCache.current.rowHeight}
+                                    rowCount={filteredItems.length}
                                     rowRenderer={({
-                                        index,
                                         key,
+                                        index,
                                         style,
                                         parent
                                     }): JSX.Element => (
                                         <CellMeasurer
                                             key={key}
-                                            cache={listCache}
+                                            cache={listCache.current}
                                             parent={parent}
                                             columnIndex={0}
                                             rowIndex={index}
                                         >
-                                            <ItemListed2
-                                                style={style}
-                                                index={index}
-                                                items={filteredItems}
-                                                addUserItem={addUserItem}
-                                                setPlayerFocused={
-                                                    setPlayerFocused
-                                                }
-                                                handleItemEditSave={
-                                                    handleItemEditSave
-                                                }
-                                            />
+                                            <div style={style}>
+                                                <ItemListed
+                                                    key={
+                                                        filteredItems[index].id
+                                                    }
+                                                    item={filteredItems[index]}
+                                                    handleItemClick={(): void => {
+                                                        addUserItem(
+                                                            filteredItems[index]
+                                                        );
+                                                        listCache.current.clearAll();
+                                                        refilterItems();
+                                                    }}
+                                                    handleItemSave={(
+                                                        item: MediaItem
+                                                    ): void => {
+                                                        setPlayerFocused(true);
+                                                        handleItemEditSave(
+                                                            item
+                                                        );
+                                                    }}
+                                                    setPlayerFocused={(
+                                                        focused: boolean
+                                                    ): void =>
+                                                        setPlayerFocused(
+                                                            focused
+                                                        )
+                                                    }
+                                                    nameEditingAllowed={true}
+                                                ></ItemListed>
+                                            </div>
                                         </CellMeasurer>
                                     )}
-                                    rowCount={filteredItems.length}
-                                    overscanRowCount={3}
                                 />
                             );
                         }}
